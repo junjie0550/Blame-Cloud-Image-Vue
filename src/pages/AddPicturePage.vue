851b69fc7e6a0840fb2dc722cfa0f7aefbbb1c18
junjie <junjie09260550@163.com>
2025-10-08 12:10:51 +0800

    feat： 图片协同编辑 WebSocket全双工通信协议 Disruptor无锁环形队列

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -28,6 +28,7 @@
           :imageUrl="picture?.url"
           :picture="picture"
           :spaceId="spaceId"
+          :space="space"
           :onSuccess="onCropSuccess"
         />
         <ImageOutPainting
@@ -83,7 +84,7 @@
 
 <script setup lang="ts">
 import PictureUpload from '@/components/PictureUpload.vue'
-import { computed, h, onMounted, reactive, ref, watch } from 'vue'
+import { computed, h, onMounted, reactive, ref, watch, watchEffect } from 'vue'
 import {
   editPictureUsingPost,
   getPictureVoByIdUsingGet,
@@ -95,6 +96,7 @@ import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
 import ImageCropper from '@/components/ImageCropper.vue'
 import { EditOutlined, FullscreenOutlined } from '@ant-design/icons-vue'
 import ImageOutPainting from '@/components/ImageOutPainting.vue'
+import { getSpaceVoByIdUsingGet } from '@/api/spaceController.ts'
 
 const router = useRouter()
 const route = useRoute()
@@ -220,6 +222,26 @@ const doImagePainting = async () => {
 const onImageOutPaintingSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture
 }
+
+// 获取空间信息
+const space = ref<API.SpaceVO>()
+
+// 获取空间信息
+const fetchSpace = async () => {
+  // 获取数据
+  if (spaceId.value) {
+    const res = await getSpaceVoByIdUsingGet({
+      id: spaceId.value,
+    })
+    if (res.data.code === 0 && res.data.data) {
+      space.value = res.data.data
+    }
+  }
+}
+
+watchEffect(() => {
+  fetchSpace()
+})
 
 </script>
 <style scoped>
